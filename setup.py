f40e3519267d31a2f4b4e15a17a4bbd619b32bb8
BlackLeakz <95303459+BlackLeakz96@users.noreply.github.com>
2022-04-16 03:35:24 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import subprocess
+import platform
+from git import Repo
+from datetime import datetime
+
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
